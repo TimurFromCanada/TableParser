699f3415603aa4a7a3ef0ea4daa78503e763254e
timurfromcanada <timurfromcanada@gmail.com>
2021-04-12 15:37:52 +0500

    Starting of the project

---
+++ QuotedFieldTask.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TableParser
+{
+    [TestFixture]
+    public class QuotedFieldTaskTests
+    {
+        [TestCase("''", 0, "", 2)]
+        [TestCase("'a'", 0, "a", 3)]
+        public void Test(string line, int startIndex, string expectedValue, int expectedLength)
+        {
+            var actualToken = QuotedFieldTask.ReadQuotedField(line, startIndex);
+            Assert.AreEqual(new Token(expectedValue, startIndex, expectedLength), actualToken);
+        }
+
+        // Добавьте свои тесты
+    }
+
+    class QuotedFieldTask
+    {
+        public static Token ReadQuotedField(string line, int startIndex)
+        {
+            return new Token(line, startIndex, line.Length - startIndex);
+        }
+    }
+}
